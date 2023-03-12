3dc4922c93476c0fd49ba5e6138c6877ebe2ad33
Valentin Flageat <63241441+vaflag@users.noreply.github.com>
2023-03-12 22:52:04 +0100

    Add data connectors

--- main.py
+++ main.py
@@ -14,9 +14,22 @@ plt.ioff()
 
 
 def upload_csv():
-    # Use the file uploader in the container
-    with st.markdown('<div class="container">'):
-        uploaded_file = st.file_uploader('Upload a file')
+    with st.container():
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            pass
+        with col2:
+            st.markdown("<h3 style='color: black;'>" +
+                        "Fetch data from Segment" + "</h3>", unsafe_allow_html=True)
+            st.image('https://images.ctfassets.net/h6ufgtwb6nv1/k6BFb1F9uVFQKOQqStRDD/ed1b10765a5350d9cd950dbf67631338/SegmentLogo_Square_Green_RGB.png', width=40)
+            st.button("Load Segment data")
+        with col3:
+            st.markdown("<h3 style='color: black;'>" +
+                        "Upload a CSV 📂" + "</h3>", unsafe_allow_html=True)
+            uploaded_file = st.file_uploader('Add your file here')
+        with col4:
+            pass
+    
     # uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         # To convert to a string based IO:
@@ -64,9 +77,11 @@ def main():
         page_title="Analytics 🪄",
         layout="wide"
     )
+    st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: black; font-size: 100px; margin-top: 100px;'>Let your data speak to you🎙</h1>",
                 unsafe_allow_html=True)
     st.set_option("deprecation.showPyplotGlobalUse", False)
+    st.markdown('#')
     data = upload_csv()
 
     if data is not None:
