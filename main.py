cc93bbc3b942451f15acaec3aef581a1342e02f7
Ali Tber <tber.ali@gmail.com>
2023-03-12 16:09:48 +0100

    Commit ismple change url and hash

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import streamlit as st
 import altair as alt
 
 from dotenv import load_dotenv
+
 load_dotenv()  # Loads environment variables from .env file
 
 
@@ -29,28 +30,36 @@ def upload_csv():
 def get_graphs(data):
     # Json sample
     data_sample = json.loads(data.head().to_json(orient="records"))
-    url = os.environ.get('DUST_APP_URL')
-    hash = os.environ.get('DUST_APP_HASH')
-    auth = 'Bearer ' + os.environ.get('DUST_APP_TOKEN')
-    headers = {
-        'Authorization': auth,
-        'Content-Type': 'application/json'
-    }
+    url = os.environ.get("DUST_APP_URL")
+    hash = os.environ.get("DUST_APP_HASH")
+    auth = "Bearer " + os.environ.get("DUST_APP_TOKEN")
+    headers = {"Authorization": auth, "Content-Type": "application/json"}
     data = {
         "specification_hash": hash,
-        "config": {"CODES": {"provider_id": "openai", "model_id": "gpt-3.5-turbo-0301", "use_cache": True}, "CATEGORY": {"provider_id": "openai", "model_id": "code-davinci-002", "use_cache": True}, "SUGGESTIONS": {"provider_id": "openai", "model_id": "code-davinci-002", "use_cache": True}},
-        'blocking': True,
-        'inputs': [{'data': data_sample}]
+        "config": {
+            "CODES": {
+                "provider_id": "openai",
+                "model_id": "gpt-3.5-turbo-0301",
+                "use_cache": True,
+            },
+            "CATEGORY": {
+                "provider_id": "openai",
+                "model_id": "code-davinci-002",
+                "use_cache": True,
+            },
+            "SUGGESTIONS": {
+                "provider_id": "openai",
+                "model_id": "code-davinci-002",
+                "use_cache": True,
+            },
+        },
+        "blocking": True,
+        "inputs": [{"data": data_sample}],
     }
-    response = requests.post(
-        url,
-        headers=headers,
-        json=data
-    )
+    response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
         response = response.json()
         return response["run"]["results"][0][0]["value"]
-
 
 
 def plot_graph(data, graph_code):
@@ -64,29 +73,29 @@ def main():
     data = upload_csv()
 
     if data is not None:
-      graphs = get_graphs(data)
-      counter = 0
-      while counter < len(graphs):
-        with st.container():
-          columns = st.columns(min(3, len(graphs) - counter))
-          for column in columns:
-            with column:
-              code = graphs[counter]['plot']
-              print('\n\n------------------\n\n')
-              print(code)
-              try:
-                plot_graph(data, code)
-              except Exception as e:
-                st.write(e)
-              counter += 1
-      return
+        graphs = get_graphs(data)
+        counter = 0
+        while counter < len(graphs):
+            with st.container():
+                columns = st.columns(min(3, len(graphs) - counter))
+                for column in columns:
+                    with column:
+                        code = graphs[counter]["plot"]
+                        print("\n\n------------------\n\n")
+                        print(code)
+                        try:
+                            plot_graph(data, code)
+                        except Exception as e:
+                            st.write(e)
+                        counter += 1
+        return
 
-      for i in range(len(graphs) / 3):
-        with st.container():
-          columns = st.columns(3)
-          for column in columns:
-             with column:
-              code="""
+        for i in range(len(graphs) / 3):
+            with st.container():
+                columns = st.columns(3)
+                for column in columns:
+                    with column:
+                        code = """
 # Convert the data to a pandas dataframe
 df = pd.DataFrame(data)
 
@@ -99,13 +108,11 @@ df = df.groupby('Date')['Weekly_Sales'].sum().reset_index()
 # Plot the graph
 st.title('Weekly sales trend over time')
 st.line_chart(df.set_index('Date'))
-"""           
-              try:
-                plot_graph(data, code)
-              except:
-                st.write("Error")
-              
-      
+"""
+                        try:
+                            plot_graph(data, code)
+                        except:
+                            st.write("Error")
 
     return
 
@@ -115,7 +122,7 @@ st.line_chart(df.set_index('Date'))
         graphs = get_graphs(data)
 
         for graph in graphs:
-            code = graph['plot']
+            code = graph["plot"]
 
             print(code)
             exec(code)
